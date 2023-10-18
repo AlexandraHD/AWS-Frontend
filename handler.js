0bf105cb5d1a281d7bda42249f2ce96327a26862
Alexandra Hernandez Duarte <alexa.her.duarte@gmail.com>
2023-10-18 13:11:04 -0500

    lambda functions

--- handler.js
+++ handler.js
@@ -1,12 +1,34 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+products = [
+  {
+    productId: 1,
+    productName: 'Book',
+    price: 123,
+  },
+  {
+    productId: 2,
+    productName: 'Pen',
+    price: 13,
+  },
+  {
+    productId: 3,
+    productName: 'Notebook',
+    price: 23,
+  }
+]
+module.exports.getProductsList = async (event) => {
   return {
     statusCode: 200,
-      body: {
-        productName: 'Book',
-        price: 123,
-      }
+    body: JSON.stringify(products)
+  };
+}
+module.exports.getProductsById = async (event) => {
+  const { id } = event.pathParameters;
+  const product = products.find((p) => p.productId === parseInt(productId));
+  return {
+    statusCode: 200,
+    body: JSON.stringify(product)
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
